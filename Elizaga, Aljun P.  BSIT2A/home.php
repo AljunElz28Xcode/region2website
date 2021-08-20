64d1b23bd1fade705760dcf0f938bb4ff5593779
AljunElz28Xcode <89255515+AljunElz28Xcode@users.noreply.github.com>
2021-08-20 21:05:02 +0900

    Add files via upload

---
+++ Elizaga, Aljun P.  BSIT2A/home.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!--website name-->
+  <title>Region II</title>
+  <!--web icon-->
+  <link rel="icon" type="image/ico" href="favicon.ico">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--read style.css-->
+  <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<header>
+   
+     
+      <nav>
+         <ul>
+            <li><a href="home.php">Home</a>
+            <li><a href="about.html">About</a>
+            <li><a href="region2.html">Region II</a>
+            <li><a href="logout.php">Logout</a>
+
+          </ul>
+         
+      </nav>
+    </header>
+
+  <body>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2><b>CAGAYAN VALLEY AGRICULTURE NEWS</b></h2>
+     <img src="i/4.png"
+         width="500px" height="350px"><div class="fakeimg2"><a href="https://www.bworldonline.com/cagayan-valley-leads-gains-in-2019-agriculture-output-by-value-volume-psa/"style="font-size: 12pt; color: blue">
+       Cagayan Valley leads gains in 2019 agriculture output by value, volume – PSA</a></div>
+    </div>
+    <div class="card">
+       
+     <img src="i/5.png"
+         width="500px" height="350px"><div class="fakeimg2"><a href="https://psa.gov.ph/content/cagayan-valley-posted-highest-increment-agricultural-production-2019-66-percent" style="font-size: 12pt; color: blue">
+       Cagayan Valley posted the highest increment in agricultural production in 2019 at 6.6 percent</a></div>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+     
+    <div class="card">
+      <h3>PHILIPPINE AGRICULTURE LATEST NEWS</h3>
+       <img src="i/1.jpg"
+         width="500px" height="100px"><div class="fakeimg2"><a href="https://www.rappler.com/voices/thought-leaders/philippine-agriculture-dying-series-what-will-take-save-it-part-one" style="font-size: 10pt; color: blue">
+         
+      [OPINION] Philippine agriculture is dying, what will it take to save it?</a></div><br>
+
+      <img src="r2/r2.jpg"
+         width="500px" height="100px"><div class="fakeimg2"><a href="https://www.philstar.com/business/2021/08/05/2117620/agriculture-groups-slam-plan-lower-corn-tariffs" style="font-size: 10pt; color: blue">
+         
+      Agriculture groups slam plan to lower corn tariffs</a></div><br>
+      
+
+        <img src="i/3.jpg"
+         width="500px" height="100px"><div class="fakeimg2"><a href="https://www.thepigsite.com/news/2021/07/philippine-department-of-ag-gives-nod-to-nutritious-gmo-golden-rice" style="font-size: 10pt; color: blue"> 
+         
+      Philippine Department of Ag gives nod to nutritious GMO 'golden rice'</a></div><br>
+
+    </div>
+  </div>
+</div>
+</div>
+<br>
+<STYLE>
+.div1 {
+  width: 100%;
+  height: 30px;
+  color: white;
+  text-align: center;
+  line-height: 30px;
+  font-family: catamaran; 
+  background-color: #555;
+}
+</STYLE>
+
+
+  </body>
+
+  <!------FOOTER------>    
+   
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="https://www.google.com/travel/things-to-do/see-all?g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4306835%2C4317915%2C4371335%2C4401769%2C4419364%2C4429192%2C4515404%2C4545890%2C4596368%2C4597339%2C4603492%2C4609526%2C47817333%2C4270859%2C4284970%2C4291517&hl=en-PH&gl=ph&ssta=1&dest_mid=%2Fm%2F01lgjg&dest_state_type=sattd&dest_src=tsvr&sa=X&ved=2ahUKEwj4jtj96r7yAhWB1GEKHenbADwQzTooATAjegQIORAC#ttdm=17.806799_119.714839_5&ttdmf=%252Fg%252F11hdjcd_zd">PLACES YOU MUST VISIT</a></li>
+                            
+                        </ul>
+                    </div>
+                    
+                    <div class="col-md-6 item text">
+                        <h3>REGION II</h3>
+                        <p>Cagayan Valley, designated as Region II, is an administrative region in the Philippines, located in the northeastern section of Luzon Island. It is composed of five Philippine provinces: Batanes, Cagayan, Isabela, Nueva Vizcaya, and Quirino</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">REGION II</p>
+            </div>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</footer>
+
+</head>
+</html>
